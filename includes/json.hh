afe5906cb180d37b6428cab66e3bd34ee9aed664
Mélanie 'bakablue' Godard <melanie.godard@gmail.com>
2015-09-27 14:22:02 +0200

    json: recode json class.
    * Can parse objects, string, arrays, integers,
    * No error handling.

--- includes/json.hh
+++ includes/json.hh
@@ -2,6 +2,10 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <boost/variant.hpp>
 
 // {}
 // { [] }
@@ -18,51 +22,64 @@
 
 namespace json
 {
-  typedef std::tuple<std::string, JsonValue> MapValue;
+  // Primitive type
+  // XXX Note: it can begin by "[]"
 
   class JsonValue
-  {
+  {  
     public:
-      virtual void parse(std::istream& stream) = 0;
+      virtual void print(std::ostream& os) const { os << "Undefined"; };
   };
 
   class Map
   {
     public:
-      void parse(std::istream& stream);
+      Map(std::ifstream& stream);
+      void print(std::ostream& os) const;
     private:
-      MapValue value_;
-  }
-  class Object : JsonValue // {}
+      // key + obj
+      void parse_key(std::ifstream& stream);
+      void parse_string(std::ifstream& stream, std::string& value);
+      void parse_number(std::ifstream& stream, double& d);
+    private:
+      std::string key_;
+      std::shared_ptr<JsonValue> value_;
+      enum MapState { KEY, VALUE } state_;
+  };
+
+  class Object : public JsonValue
   {
     public:
-      void parse(std::istream& stream);
+      Object(std::ifstream& stream);
+      void print(std::ostream& os) const;
     private:
-      std::vector<Map> data_;
+      std::vector<Map> elts_;
   };
-  class Array : JsonValue // []
+
+  class Array : public JsonValue
   {
     public:
-      void parse(std::istream& stream);
+      Array(std::ifstream& stream);
+      void print(std::ostream& os) const;
     private:
-      std::vector<Object> data_;
+      std::vector<Object> objects_;
   };
 
-  // Primitive types
-  class PrimitiveType {};
 
-  class String : PrimitiveType
+  typedef boost::variant<std::string, double> ptype;
+
+  class PrimitiveValue : public JsonValue
   {
+
+    public:
+      PrimitiveValue(double      value);
+      PrimitiveValue(std::string value);
+
+      ptype getValue() const { return value_; };
+      void print(std::ostream& os) const;
+    private:
+      ptype value_;
   };
 
-  class Boolean : PrimitiveType
-  {
-  };
-
-  class NullValue {};
-
-  class Integer : PrimitiveType
-  {
-  };
-
+  std::ostream& operator<<(std::ostream& os, const JsonValue& pvalue);
 };
--- src/json.cc
+++ src/json.cc
@@ -3,69 +3,187 @@
 // TODO Test with an empty string
 namespace json
 {
-      void Object::parse(std::istream& stream)
+  Map::Map(std::ifstream& stream)
+    : key_("")
+    , state_(KEY)
+  {
+    //std::cout << "begin Map" << stream.tellg() << std::endl;
+    do {
+      char c = stream.peek();
+
+      if (c == ',' || c == '}')
+        break;
+
+      stream.get();
+
+      if (c == '"')
       {
-        char c;
-        if (stream.get() == '{')
-        {
-          while ((c = stream.peek()) == '}')
-          {
-            if (c == ' ')
-              c << stream;
-            else if (c == '"')
-            {
-              
-            }
-          }
-        }
+        if (state_ == KEY)
+          parse_string(stream, key_);
         else
         {
-          // error
+          std::string str;
+          parse_string(stream, str);
+          value_ = std::make_shared<PrimitiveValue>(PrimitiveValue(str));
         }
       }
 
-      void Map::parse(std::istream& stream)
+      else if (std::isdigit(c))  // And state == VALUE or else -> error
       {
-        char c;
-        String s;
-
-        // Parse String
-        do {
-          c << stream;
-        } while (c == ' ' || c == '\n');
-
-        if (c == "")
-        {
-          if (!s.parse(stream))
-            ; // Error
-        }
-
-        else
-          ;// Error
-
-        // Parse JsonValue
-        while ((c = stream.get()) != ',' || c != '}')
-        {
-          if (c == '"')
-          {
-            String s;
-            s.parse(stream);
-          }
-          else if (isdigit(c))
-          {
-            Integer i;
-            i.parse(stream);
-          }
-          else if (c == '{')
-          {
-            Object o;
-            o.parse(stream);
-          }
-          else if (c == '[')
-          {
-            Array a;
-            a.parse(stream);
-          }
-        }
+        stream.unget();
+        double i;
+        parse_number(stream, i);
+        value_ = std::make_shared<PrimitiveValue>(PrimitiveValue(i));
       }
+      else if (c == '-')
+      {
+        double i;
+        parse_number(stream, i);
+        value_ = std::make_shared<PrimitiveValue>(PrimitiveValue(-1 * i));
+      }
+      else if (c == '{')
+      {
+        //std::cout << "found object " << stream.tellg() << std::endl;
+        value_ = std::make_shared<Object>(Object(stream));
+      }
+      else if (c == '[')
+      {
+        //std::cout << "found array " << stream.tellg() << std::endl;
+        value_ = std::make_shared<Array>(Array(stream));
+      }
+
+      else if (c == ':')
+        state_ = VALUE;
+      // object
+      // map
+    } while (!stream.eof());
+    //std::cout << "end Map"  << stream.tellg()<< std::endl;
+  }
+
+  void Map::parse_key(std::ifstream& stream)
+  {
+    parse_string(stream, key_);
+  }
+
+  void Map::parse_string(std::ifstream& stream, std::string& value)
+  {
+    char c;
+    while ((c = stream.get()) != '"')
+    {
+      value += c;
+    }
+  }
+
+  void Map::parse_number(std::ifstream& stream, double& value)
+  {
+    std::string number_str;
+    char c;
+
+    do {
+      c = stream.get();
+      number_str += c;
+    } while (std::isdigit(c) || c == '.');
+
+    stream.unget();
+    value = std::atof(number_str.c_str());
+  }
+
+  void Map::print(std::ostream& os) const
+  {
+    os << key_ <<  " : " << *value_;
+  }
+
+  Object::Object(std::ifstream& stream)
+  {
+    //std::cout << "begin Object" << stream.tellg() << std::endl;
+    char c;
+    do
+    {
+      c = stream.peek();
+
+      if (c != ' ' || c != '\n')
+        elts_.push_back(Map(stream));
+
+      c = stream.get();
+
+    } while (c != '}');
+    //std::cout << "end Object" << stream.tellg() << std::endl;
+  }
+
+  void Object::print(std::ostream& os) const
+  {
+    bool first = true;
+
+    os << "{ ";
+
+    for (auto m : elts_)
+    {
+      if (first)
+        first = false;
+      else
+        os << ", ";
+      m.print(os);
+    }
+
+    os << " }";
+  }
+
+  Array::Array(std::ifstream& stream)
+  {
+    //std::cout << "begin Array" << stream.tellg() << std::endl;
+    char c;
+    do
+    {
+      c = stream.peek();
+
+      if (c == '{')
+      {
+        stream.get();
+        objects_.push_back(Object(stream));
+      }
+
+      c = stream.get();
+
+    } while (c != ']');
+    //std::cout << "end Array" << stream.tellg() << std::endl;
+  }
+
+  void Array::print(std::ostream& os) const
+  {
+    bool first = true;
+
+    os << "[ ";
+    for (auto o : objects_)
+    {
+      if (first)
+        first = false;
+      else
+        os << ", ";
+      os << o;
+
+    }
+    os << " ]";
+  }
+
+  PrimitiveValue::PrimitiveValue(double value)
+    : value_(value)
+  {
+  }
+
+  PrimitiveValue::PrimitiveValue(std::string value)
+    : value_(value)
+  {
+  }
+
+  void PrimitiveValue::print(std::ostream& os) const
+  {
+    os << value_;
+  }
+
+
+  std::ostream& operator<<(std::ostream& os, const JsonValue& pvalue)
+  {
+    pvalue.print(os);
+    return os;
+  }
 }
